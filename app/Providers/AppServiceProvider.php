b755c5bf53481e2734cdaf9e80c3619385654ad7
Dryn2007 <adrianadiputra2007@gmail.com>
2026-03-23 23:51:02 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- 1. Tambahkan ini
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // 2. Tambahkan kode ini agar CSS tidak diblokir browser
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
