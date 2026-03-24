ff4aedbe83f5b0af6a2204477e3fe330ef87cc22
Dryn2007 <adrianadiputra2007@gmail.com>
2026-03-24 13:19:26 +0700

    ada

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}"
                         class="flex items-center gap-3 group transition-transform active:scale-95">
-                        <img src="{{ asset('images/Thumbnail.jpg') }}" alt="Keluarga H Solichin"
+                        <img src="{{ asset('images/logo.webp') }}" alt="Keluarga H Solichin"
                             class="h-10 w-10 rounded-full shadow-sm group-hover:shadow-md transition-all object-cover">
                         <span class="font-extrabold text-lg tracking-tight text-gray-800 hidden sm:block">
                             Keluarga <span class="text-teal-600">H Solichin</span>
